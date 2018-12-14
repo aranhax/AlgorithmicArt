66e3a0a0ce5d1518d4bd52df9b9e2fa33ffc0408
Aranhax <proj.brain@gmail.com>
2018-12-14 11:56:30 -0800

    Added code to generate the figures

--- CA/wolfram.py
+++ CA/wolfram.py
@@ -8,6 +8,8 @@ https://en.wikipedia.org/wiki/A_New_Kind_of_Science
 https://github.com/brahmcapoor/cellular-automata
 https://github.com/zmwangx/rule30/blob/master/rule30/automaton.py
 https://faingezicht.com/articles/2017/01/23/wolfram/
+https://stackoverflow.com/questions/12062920/how-do-i-create-an-image-in-pil-using-a-list-of-rgb-tuples
+Draw something (http://stackoverflow.com/a/10032271/562769)
 
 @author: aranildo
 """
@@ -15,6 +17,7 @@ https://faingezicht.com/articles/2017/01/23/wolfram/
 
 import numpy as np
 import itertools as it
+import matplotlib.pyplot as plt
 
 def states(n_generations=100, n_cells=None, initial_state = 'mid'):
     if n_cells is None:
@@ -41,14 +44,31 @@ def define_rule(rulenumer):
     return all_rules    
 
 ca_rules = define_rule(150)
-ss = states(45)    
+ss = states(250, 250)    
 n_cells = ss.shape[1]
 
-print(''.join(map(str, ss[0,:]))) 
+
+save_path_figures = "~/temp/art/gif/"
+
+# Image size
+width = n_cells
+height = n_cells
+#channels = 1
+channels = 3
+# Create an empty image
+img = np.zeros((height, width, channels), dtype=np.uint8)
+
+img[0][:] = np.repeat(np.expand_dims(ss[0,:]*255, axis=1) , 3, axis=1)
 for j,generation in enumerate(ss[:-1,:]):
+    plt.figure()
     new_generation = [ca_rules[tuple([generation[len(generation) - 1], generation[0], generation[1]])]]
     _  =   [new_generation.append(ca_rules[tuple([generation[i - 1], generation[i], generation[i + 1]])]) for i in range(1,n_cells-1)]
-    new_generation.append(ca_rules[tuple([generation[len(generation) - 2], generation[len(generation) - 1], generation[0]])])
-    print(''.join(map(str, new_generation))) 
+    new_generation.append(ca_rules[tuple([generation[len(generation) - 2], generation[len(generation) - 1], generation[0]])])    
+    img[j][:] = np.repeat(np.expand_dims([x * 255 for x in new_generation] , axis=1), 3, axis=1)
     ss[j+1,:] = new_generation
+    plt.imshow(img)
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig(save_path_figures + 'Frame' + str(j).zfill(3) + '.jpeg', format='jpeg')
+    plt.close('all')
 
