83aa7da3404fbe599332ad5c578f260bf73c76a8
Aranhax <proj.brain@gmail.com>
2019-06-22 19:58:30 -0700

    First try  CA with colour

--- CA/colour.py
+++ CA/colour.py
@@ -17,12 +17,13 @@ http://stackoverflow.com/a/10032271/562769
 import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
-from statistics import mode
+import scipy.misc as smp
 
 def states(n_generations=50, size=(10,10)):    
     actual_row = np.random.randint(0, 3, size=size)    
-    spacetime = np.zeros(size + (n_generations,),dtype=np.int8)
-    spacetime[:,:,0] = actual_row
+    #spacetime = np.zeros(size + (n_generations,),dtype=np.int8)
+    spacetime = np.zeros(size ,dtype=np.int8)
+    spacetime[:,:] = actual_row
 
     return spacetime
 
@@ -30,70 +31,65 @@ def states(n_generations=50, size=(10,10)):
 def define_rule(element,size, time):
     
     if element[0] == size[0]-1:
-        vote = mode([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],element[1]+1,time],  spacetime[0,element[1],time]])
+        vote = find_majority([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],element[1]+1,time],  spacetime[0,element[1],time]])
     elif element[1] == size[1]-1:
-        vote = mode([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],0,time], spacetime[element[0]+1,element[1],time]])
+        vote = find_majority([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],0,time], spacetime[element[0]+1,element[1],time]])
     elif (element[0] == size[0]-1) and (element[1] == size[1]-1):
-        vote = mode([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],0,time], spacetime[0,element[1],time]])
+        vote = find_majority([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],0,time], spacetime[0,element[1],time]])
     else:
-        vote = mode([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],element[1]+1,time], spacetime[element[0]+1,element[1],time]])
+        vote = find_majority([spacetime[element[0]-1,element[1],time], spacetime[element[0],element[1]-1,time], spacetime[element[0],element[1]+1,time], spacetime[element[0]+1,element[1],time]])
         
     return vote
 
+def define_rule_notime(spacetime, element):    
+    size = spacetime.shape
+    if (element[0] == size[0]-1) and (element[1] == size[1]-1):
+        vote = find_majority([spacetime[element[0]-1,element[1]], spacetime[element[0],element[1]-1], spacetime[element[0],0], spacetime[0,element[1]]])
+    elif element[0] == size[0]-1:
+        vote = find_majority([spacetime[element[0]-1,element[1]], spacetime[element[0],element[1]-1], spacetime[element[0],element[1]+1],  spacetime[0,element[1]]])
+    elif element[1] == size[1]-1:
+        vote = find_majority([spacetime[element[0]-1,element[1]], spacetime[element[0],element[1]-1], spacetime[element[0],0], spacetime[element[0]+1,element[1]]])
+    else:
+        vote = find_majority([spacetime[element[0]-1,element[1]], spacetime[element[0],element[1]-1], spacetime[element[0],element[1]+1], spacetime[element[0]+1,element[1]]])
+        
+    return vote
 
+from collections import Counter
 
+def find_majority(votes):
+    vote_count = Counter(votes)
+    top_two = vote_count.most_common(2)
+    if len(top_two)>1 and top_two[0][1] == top_two[1][1]:
+        # It is a tie
+        return 0
+    return top_two[0][0]
 
-
-n_generations=100
-rule = 150
-
-ca_rules = define_rule(rule)
-
-ss = states(n_generations)
-
-
-n_cells = ss.shape[1]
 
 save_path_figures = "/home/aranildo/temp/art/gif/test/"
 
-import numpy as np
-import scipy.misc as smp
+
+width = 1920
+height = 1080
 
 # Create a 1024x1024x3 array of 8 bit unsigned integers
-data = np.zeros( (1024,1024,3), dtype=np.uint8 )
+data_img = np.zeros((height,width,3), dtype=np.uint8 )
+ss = states(size=(height,width))
+# serial
+for t in range(300):
+    for t2 in range(width*20):#*height):
+        elem = [np.random.randint(0, height),np.random.randint(0, width)]
+        ss[elem[0],elem[1]] = define_rule_notime(ss,(elem[0],elem[1]))
+        
+    data_img[ss == 0] = [255,0,0] 
+    data_img[ss == 1] = [0,255,0] 
+    data_img[ss == 2] = [0,0,255] 
+    
+    img = smp.toimage(data_img)       # Create a PIL image
+    #if t//100: 
+    img.save(save_path_figures + 'Frame' + str(t+1).zfill(3) + '.jpeg')
 
-data[512,512] = [0,255,0]       # Makes the middle pixel red
-data[512,513] = [0,0,255]       # Makes the next pixel blue
 
-img = smp.toimage( data )       # Create a PIL image
 img.show()      
 
 
 # Image size
-width = n_cells
-height = n_generations
-#channels = 1
-channels = 3
-# Create an empty image
-img = np.zeros((10, 10, channels), dtype=np.uint8)
-
-img[0][:] = np.repeat(np.expand_dims(ss[0,:,0] * 255, axis=1) , 3, axis=1)
-
-for j,generation in enumerate(ss[:-1,:]):
-    #plt.rcParams["figure.figsize"] = (width,height)
-    #plt.rcParams["figure.dpi"] = 100
-    plt.rcParams["savefig.transparent"]=True
-    plt.rcParams["savefig.bbox"]='tight'
-    plt.rcParams["savefig.edgecolor"] = 'black'
-    plt.rcParams["savefig.facecolor"] = 'black'
-
-    new_generation = [ca_rules[tuple([generation[len(generation) - 1], generation[0], generation[1]])]]
-    _  =   [new_generation.append(ca_rules[tuple([generation[i - 1], generation[i], generation[i + 1]])]) for i in range(1,n_cells-1)]
-    new_generation.append(ca_rules[tuple([generation[len(generation) - 2], generation[len(generation) - 1], generation[0]])])    
-    img[j][:] = np.repeat(np.expand_dims([x * 255 for x in new_generation] , axis=1), 3, axis=1)
-    ss[j+1,:] = new_generation
-    plt.imshow(img)
-    plt.xticks([])
-    plt.yticks([])
-    plt.savefig(save_path_figures + 'Frame' + str(j+1).zfill(3) + '.jpeg', format='jpeg')
-    plt.close('all')
